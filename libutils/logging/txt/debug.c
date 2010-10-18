62b223faaa580a81eabcfd6ac0e0b27f84ceb08e
Eric A. Welsh <Eric.Welsh@moffitt.org>
2010-10-18 17:16:05 -0400

    Initial timestamps commit;
    ADD:  libutils/logging/txt/debug.c;
          [Mon Oct 18 17:16:05 2010 -0400]

---
+++ libutils/logging/txt/debug.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+#include "util_log.h"
+
+void debug(const char *msg, ...)
+{
+  va_list ap;
+
+  va_start(ap, msg);
+  vfprintf(stderr, msg, ap);
+  va_end(ap);
+
+  fprintf(stderr, "\n");
+}
