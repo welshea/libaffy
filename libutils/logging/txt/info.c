4431ded5b43531cf79fc8da8f8a38aa1eab9a9b8
Eric A. Welsh <Eric.Welsh@moffitt.org>
2010-10-18 17:16:05 -0400

    Initial timestamps commit;
    ADD:  libutils/logging/txt/info.c;
          [Mon Oct 18 17:16:05 2010 -0400]

---
+++ libutils/logging/txt/info.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+#include "util_log.h"
+
+void info(const char *msg, ...)
+{
+  va_list ap;
+
+  va_start(ap, msg);
+
+  vfprintf(stderr, msg, ap);
+
+  va_end(ap);
+
+  fprintf(stderr, "\n");
+}
