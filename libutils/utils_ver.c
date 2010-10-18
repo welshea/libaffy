1c8ed23685bbddc8b57fd93357c60acba7585294
Eric A. Welsh <Eric.Welsh@moffitt.org>
2010-10-18 17:16:05 -0400

    Initial timestamps commit;
    ADD:  libutils/utils_ver.c;
          [Mon Oct 18 17:16:05 2010 -0400]

---
+++ libutils/utils_ver.c
@@ -0,0 +1,18 @@
+
+/* 
+ *  utils_ver.c
+ *
+ *  Adapted from the original libutils, written by:
+ *
+ *       Steven Eschrich
+ *	 eschrich@csee.usf.edu
+ *	 
+ *  ANSI-ified by Andrew M. Hoerter.  Where possible I have kept to the
+ *  original libutils interfaces, but in some cases changes have been
+ *  made to make the code standards-conforming.  Any resulting bugs are
+ *  mine alone.
+ *
+ */
+
+const char libutils_version[] = "libutils, development version ($Rev: 223 $)";
+
