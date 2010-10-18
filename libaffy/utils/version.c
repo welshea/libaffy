478fd179ea08be132a41623dc03e2bbe026db90e
Eric A. Welsh <Eric.Welsh@moffitt.org>
2010-10-18 17:16:04 -0400

    Initial timestamps commit;
    ADD:  libaffy/utils/version.c;
          [Mon Oct 18 17:16:04 2010 -0400]

---
+++ libaffy/utils/version.c
@@ -0,0 +1,23 @@
+
+/**************************************************************************
+ *
+ * Filename:  version.c
+ *
+ * Purpose:   Provide library version information.
+ *
+ * Creation:  05/07/09
+ *
+ * Author:    Andrew M Hoerter
+ *
+ * Copyright: Copyright (C) 2009, Moffitt Cancer Center 
+ *            All rights reserved. 
+ *
+ * Update History
+ * --------------
+ * 05/07/09: Creation (AMH)
+ *
+ **************************************************************************/
+
+#include <affy_version.h>
+
+const char affy_version[] = "libaffy revision " AFFY_REVISION;
