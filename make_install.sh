b6c2774c4370f7909becc1c6be9f0445dd5a61e3
Eric A. Welsh <Eric.Welsh@moffitt.org>
2019-04-25 12:33:15 -0400

    Initial timestamps commit;
    ADD:  make_install.sh;
          [Thu Apr 25 12:33:15 2019 -0400]

---
+++ make_install.sh
@@ -0,0 +1,23 @@
+mkdir bin
+
+cp -a affy-apps/scripts/agilent_to_spreadsheet.pl bin
+
+cp -a affy-apps/affydump/affydump.exe bin
+cp -a affy-apps/calvindump/calvindump.exe bin
+cp -a affy-apps/findmedian/findmedian.exe bin
+cp -a affy-apps/iron/iron.exe bin
+cp -a affy-apps/iron_generic/iron_generic.exe bin
+cp -a affy-apps/mas5/mas5.exe bin
+cp -a affy-apps/pairgen/pairgen.exe bin
+cp -a affy-apps/rma/rma.exe bin
+#cp -a affy-apps/datExtractor/datExtractor.exe bin
+
+cp -a affy-apps/affydump/affydump bin
+cp -a affy-apps/calvindump/calvindump bin
+cp -a affy-apps/findmedian/findmedian bin
+cp -a affy-apps/iron/iron bin
+cp -a affy-apps/iron_generic/iron_generic bin
+cp -a affy-apps/mas5/mas5 bin
+cp -a affy-apps/pairgen/pairgen bin
+cp -a affy-apps/rma/rma bin
+#cp -a affy-apps/datExtractor/datExtractor bin
