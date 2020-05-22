9cf94175a74a62d96c5e8bcc1fa3513ae4b119ac
Eric A. Welsh <Eric.Welsh@moffitt.org>
2020-05-22 17:04:48 -0400

    Initial timestamps commit;
    ADD:  libutils/text/mempcpy.c;
          [Fri May 22 17:04:48 2020 -0400]

---
+++ libutils/text/mempcpy.c
@@ -0,0 +1,38 @@
+/* Copy memory area and return pointer after last written byte.
+   Copyright (C) 2003, 2007 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
+
+/* Changelog:
+ *
+ * 2020-04-30   don't define it when we shouldn't (EAW)
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#if !defined _GNU_SOURCE && HAVE_MEMPCPY == 0
+
+#include <string.h>
+
+/* Copy N bytes of SRC to DEST, return pointer to bytes after the
+   last written byte.  */
+void *mempcpy (void *dest, const void *src, size_t n)
+{
+  return (char *) memcpy (dest, src, n) + n;
+}
+#endif
