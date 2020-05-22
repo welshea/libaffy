298f37e1d4b086f71d7663d0daaabb33520cc18d
Eric A. Welsh <Eric.Welsh@moffitt.org>
2020-05-22 17:10:29 -0400

    Initial timestamps commit;
    ADD:  libutils/sys_config.h;
          [Fri May 22 17:10:29 2020 -0400]

---
+++ libutils/sys_config.h
@@ -0,0 +1,17 @@
+#ifndef LIBUTILS_SYS_CONFIG_H_SEEN
+#define LIBUTILS_SYS_CONFIG_H_SEEN
+
+
+/* Set to 1 if strdup is defined. */
+#define HAVE_DECL_STRDUP 1
+
+/* Set to 1 if strcasecmp is defined. */
+#define HAVE_DECL_STRCASECMP 1
+
+/* Set to 1 if strnlen is defined. */
+#define HAVE_DECL_STRNLEN 1
+
+/* Set to 1 if strndup is defined. */
+#define HAVE_DECL_STRNDUP 1
+
+#endif /* LIBUTILS_SYS_CONFIG_H_SEEN */
