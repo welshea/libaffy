e2b82a8d5569a1282f9d37e90020093cc3a49069
Eric A. Welsh <Eric.Welsh@moffitt.org>
2020-08-18 17:22:34 -0400

    minor output format changes, flags to enable/disable probeset normalization;
    MOD:  libaffy/mas5/mas5_set_defaults.c;
          [Tue Aug 18 17:22:34 2020 -0400]

--- libaffy/mas5/mas5_set_defaults.c
+++ libaffy/mas5/mas5_set_defaults.c
@@ -27,6 +27,9 @@
  * 10/04/17: added iron_condense_training (EAW)
  * 06/01/18: added support for probeset exclusions during IRON training (EAW)
  * 09/14/18: added spikeins support, similar to exclusions (EAW)
+ * 08/12/20: add cdf_filename (EAW)
+ * 08/18/20: add flags to enable/disable iron or quantile probeset norm after
+ *           probe norm (EAW)
  *
  **************************************************************************/
 
@@ -44,6 +47,7 @@ void affy_mas5_set_defaults(AFFY_COMBINED_FLAGS *f)
   f->contrast_tau = 0.03;
   f->scale_tau = 10;
   f->cdf_directory = ".";
+  f->cdf_filename = "";
   f->probe_filename = "probe-values.txt";
   f->dump_probe_values = false;
   f->output_present_absent = false;
@@ -81,4 +85,5 @@ void affy_mas5_set_defaults(AFFY_COMBINED_FLAGS *f)
   f->use_spikeins = false;
   f->spikeins_filename = NULL;
   f->ignore_chip_mismatch = false;
+  f->normalize_probesets = false;
 }
