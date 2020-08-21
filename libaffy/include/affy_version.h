481aa6d88e8acaacd67e92a6de30761ea142774b
Eric A. Welsh <Eric.Welsh@moffitt.org>
2020-08-21 13:29:30 -0400

    prepare new release version;
    MOD:  libaffy/include/affy_version.h;
          [Fri Aug 21 13:29:30 2020 -0400]

--- libaffy/include/affy_version.h
+++ libaffy/include/affy_version.h
@@ -1,7 +1,7 @@
 
 #ifndef _AFFY_VERSION_H_
 #define _AFFY_VERSION_H_
-#define AFFY_REVISION "build date: 2020-08-19 17:06:34"
+#define AFFY_REVISION "build date: 2020-08-21 13:29:30"
 extern const char affy_version[];
 #endif
 
