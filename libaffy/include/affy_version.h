9a77a87b02b8831644ee0f30c6d2b3413b7d720b
Eric A. Welsh <eric.welsh@moffitt.org>
2021-05-25 13:41:40 -0400

    fix crash on (nearly) empty samples;
    MOD:  libaffy/include/affy_version.h;
          [Tue May 25 13:41:40 2021 -0400]

--- libaffy/include/affy_version.h
+++ libaffy/include/affy_version.h
@@ -1,7 +1,7 @@
 
 #ifndef _AFFY_VERSION_H_
 #define _AFFY_VERSION_H_
-#define AFFY_REVISION "build date: 2021-03-24 12:20:47"
+#define AFFY_REVISION "build date: 2021-05-25 13:41:40"
 extern const char affy_version[];
 #endif
 
