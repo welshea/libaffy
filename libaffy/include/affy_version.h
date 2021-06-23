99ae9bda790f97e55ebaa1c896015aba0ba5087a
Eric A. Welsh <eric.welsh@moffitt.org>
2021-06-23 14:00:11 -0400

    change probe ControlType check to treat 0 as non-control;
    MOD:  libaffy/include/affy_version.h;
          [Wed Jun 23 14:00:11 2021 -0400]

--- libaffy/include/affy_version.h
+++ libaffy/include/affy_version.h
@@ -1,7 +1,7 @@
 
 #ifndef _AFFY_VERSION_H_
 #define _AFFY_VERSION_H_
-#define AFFY_REVISION "build date: 2021-05-25 13:41:40"
+#define AFFY_REVISION "build date: 2021-06-23 14:00:11"
 extern const char affy_version[];
 #endif
 
