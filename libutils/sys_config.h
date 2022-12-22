f9a42c63f336a5d28137e0fe7ed5695ab89427ab
Eric A. Welsh <eric.welsh@moffitt.org>
2022-12-22 10:48:58 -0500

    should build on Ubuntu now;
    MOD:  libutils/sys_config.h;
          [Thu Dec 22 10:48:58 2022 -0500]

--- libutils/sys_config.h
+++ libutils/sys_config.h
@@ -3,15 +3,18 @@
 
 
 /* Set to 1 if strdup is defined. */
-#define HAVE_DECL_STRDUP 1
+/* #undef HAVE_DECL_STRDUP */
 
 /* Set to 1 if strcasecmp is defined. */
-#define HAVE_DECL_STRCASECMP 1
+/* #undef HAVE_DECL_STRCASECMP */
 
 /* Set to 1 if strnlen is defined. */
-#define HAVE_DECL_STRNLEN 1
+/* #undef HAVE_DECL_STRNLEN */
 
 /* Set to 1 if strndup is defined. */
-#define HAVE_DECL_STRNDUP 1
+/* #undef HAVE_DECL_STRNDUP */
+
+/* Set to 1 if argp_parse is defined. */
+/* #undef HAVE_DECL_ARGP_PARSE */
 
 #endif /* LIBUTILS_SYS_CONFIG_H_SEEN */
