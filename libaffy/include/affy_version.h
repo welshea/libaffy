8d3d0fc00cbc277431c8a68a8ac42d8ed354e546
Eric A. Welsh <eric.welsh@moffitt.org>
2022-12-22 10:48:58 -0500

    should build on Ubuntu now;
    MOD:  libaffy/include/affy_version.h;
          [Thu Dec 22 10:48:58 2022 -0500]

--- libaffy/include/affy_version.h
+++ libaffy/include/affy_version.h
@@ -1,7 +1,7 @@
 
 #ifndef _AFFY_VERSION_H_
 #define _AFFY_VERSION_H_
-#define AFFY_REVISION "build date: 2021-06-23 14:00:11"
+#define AFFY_REVISION "build date: 2022-12-22 10:48:58"
 extern const char affy_version[];
 #endif
 
