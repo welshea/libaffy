6663aa39002e15d6eed91b55fb51a879f271c57b
Eric A. Welsh <eric.welsh@moffitt.org>
2024-03-25 16:55:38 -0400

    improve -m behavior, add various --floor flags, --rnaseq STDERR headers;
    MOD:  libaffy/include/affy_version.h;
          [Mon Mar 25 16:55:38 2024 -0400]

--- libaffy/include/affy_version.h
+++ libaffy/include/affy_version.h
@@ -1,7 +1,7 @@
 
 #ifndef _AFFY_VERSION_H_
 #define _AFFY_VERSION_H_
-#define AFFY_REVISION "build date: 2023-09-13 13:27:41"
+#define AFFY_REVISION "build date: 2024-03-25 16:55:38"
 extern const char affy_version[];
 #endif
 
