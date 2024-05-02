6f07516e6812d93231691a77333804a3a556b2b7
Eric A. Welsh <eric.welsh@moffitt.org>
2024-05-01 22:12:52 -0400

    add --norm-median --mnorm-include-min -mnorm-exclude-min;
    MOD:  libaffy/include/affy_version.h;
          [Wed May  1 22:12:52 2024 -0400]

--- libaffy/include/affy_version.h
+++ libaffy/include/affy_version.h
@@ -1,7 +1,7 @@
 
 #ifndef _AFFY_VERSION_H_
 #define _AFFY_VERSION_H_
-#define AFFY_REVISION "build date: 2024-03-25 16:55:38"
+#define AFFY_REVISION "build date: 2024-05-01 22:12:52"
 extern const char affy_version[];
 #endif
 
